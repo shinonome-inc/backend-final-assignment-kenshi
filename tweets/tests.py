66f48b7ffd7ed30bd58940837280fe88a19c327b
kenshi <k.minamiya@4nonome.com>
2022-12-29 15:37:56 +0900

    SignUp

--- tweets/tests.py
+++ tweets/tests.py
@@ -9,40 +9,10 @@ class TestHomeView(TestCase):
     def setUp(self):
         self.url = reverse("accounts:signup")
 
-    def test_success_post(self):
-        valid_data = {
-            "username": "testuser",
-            "email": "test@test.com",
-            "password1": "testpassword",
-            "password2": "testpassword",
-        }
-        response = self.client.post(self.url, valid_data)
-
-        self.assertRedirects(
-            response,
-            reverse("tweets:home"),
-            status_code=302,
-            target_status_code=200,
-        )
-        self.assertTrue(User.objects.filter(username=valid_data["username"]).exists())
-        self.assertIn(SESSION_KEY, self.client.session)
-
-    def test_failure_post_with_empty_username(self):
-        invalid_data = {
-            "username": "",
-            "email": "test@test.com",
-            "password1": "testpassword",
-            "password2": "testpassword",
-        }
-        response = self.client.post(self.url, invalid_data)
-        form = response.context["form"]
-
+    def test_success_get(self):
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
-        self.assertFalse(
-            User.objects.filter(username=invalid_data["username"]).exists()
-        )
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors["username"], ["このフィールドは必須です。"])
+        self.assertTemplateUsed(response, "accounts/signup.html")
 
 
 class TestTweetCreateView(TestCase):
